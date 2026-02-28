a3af4a99f913205a30ca6d959d254506afa8e31f
asraful.official00 <asraful.official00@gmail.com>
2026-02-28 21:37:33 +0000

    Updated project files

---
+++ app/src/main/java/com/example/philosophy/data/Subject.kt
@@ -0,0 +1,16 @@
+
+package com.example.philosophy.data
+
+data class Suggestion(
+    val id: String,
+    val question: String,
+    val answer: String?
+)
+
+data class PhilosophySubject(
+    val subjectCode: String,
+    val subjectName: String,
+    val partA: List<Suggestion>,
+    val partB: List<Suggestion>,
+    val partC: List<Suggestion>
+)
